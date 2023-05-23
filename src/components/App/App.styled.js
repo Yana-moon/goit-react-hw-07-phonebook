995cbdbebffb0f3f6c058ac504e63bc48600790e
Яна <112972800+Yana-moon@users.noreply.github.com>
2023-05-23 14:17:13 +0300

    added old styles in App

--- src/components/App/App.styled.js
+++ src/components/App/App.styled.js
@@ -2,17 +2,26 @@ import styled from 'styled-components';
 
 
 export const Wrapper = styled.div`
-  flex-grow:1;
-  padding: 33px;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: baseline;
+gap: 40px;
+margin: 10px auto;
+padding: 10px;
+width: 600px;
+min-height: 300px;
+max-height: max-content;
+border-radius: 4px;
   background-color:  #f2ea94;
 `;
 
 export const SectionsContainer = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 40px;
-  justify-content: center;
-  align-items: center;  
+display: flex;
+flex-wrap: wrap;
+gap: 40px;
+justify-content: center;
+align-items: center; 
     
 `;
 
